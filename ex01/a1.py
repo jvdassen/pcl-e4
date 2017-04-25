58c1553eaa7b580802cf8f77ec7c772a9387e87a
lucBoillat <luc.boillat@outlook.com>
2017-04-25 21:05:10 +0200

    a1 and requirements

--- ex01/a1.py
+++ ex01/a1.py
@@ -1,36 +1,44 @@
+#!/usr/bin/python
+#Luc Boillat 14-715-577
+#Jan von der Assen 14-719-132
+
 import glob
 import lxml.etree as ET
-import os
 
 def getfreqwords(indir, outfile):
-
-    filesize = 0
+    """
+    opens all xml files one by one and parses them with lxml. every sentence of the books
+    gets lemmatized and hashed and added to a dictionary, together with the number of
+    occurences of that sentence in all the files. When all files have been iterated through,
+    the program iterates through the files again, to find the actual sentences, based
+    on the hashes. These sentences are then written to a file.
+    """
     hashed_sents = {}
+    #finds all files that end in mul.xml
     for xml_file in glob.glob(indir + '/*mul.xml'):
 
         with open(xml_file, encoding="utf-8") as xmlfile:
-            filesize += os.path.getsize(xml_file)
-            tree = ET.parse(xmlfile)
-            root = tree.getroot()
-
-            for article in root:
-                for content in article:
-                    for sentence in content.findall("s"):
-                        lemma_sentence = lemmatize_sent(sentence)
-                        hashed_sentence = hash(lemma_sentence)
-                        if hashed_sentence in hashed_sents:
-                            hashed_sents[hashed_sentence] += 1
-                        elif hashed_sentence not in hashed_sents and len(sentence.findall("w")) > 5:
-                            hashed_sents[hashed_sentence] = 1
-                        else:
-                            pass
-    print(filesize)
+            #parses the file with lxml
+            for _, sentence in ET.iterparse(xml_file, tag='s'):
+                lemma_sentence = lemmatize_sent(sentence)
+                hashed_sentence = hash(lemma_sentence)
+                if hashed_sentence in hashed_sents:
+                    hashed_sents[hashed_sentence] += 1
+                elif hashed_sentence not in hashed_sents and len(sentence.findall("w")) > 5:
+                    hashed_sents[hashed_sentence] = 1
+                else:
+                    pass
+                #clears the node
+                sentence.clear()
+    #the dicionary get sorted and written to a list
     sentence_list = sorted(hashed_sents.items(), key=lambda x: x[1])
     hashed_sents.clear()
 
+    #the list gets shortened
     short_sentence_list = sentence_list[-20:]
     sentence_list = []
 
+    #the shortened list gets transferred into a dictionary again
     short_sentence_dic = {}
     for item in short_sentence_list:
         short_sentence_dic[item[0]] = item[1]
@@ -38,33 +46,36 @@ def getfreqwords(indir, outfile):
 
     final_sentences = {}
 
+    #it iterates once again through the files and lines
     for xml_file in glob.glob(indir + '/*mul.xml'):
 
         with open(xml_file, encoding="utf-8") as xmlfile:
-            tree = ET.parse(xmlfile)
-            root = tree.getroot()
-
-            for article in root:
-                for content in article:
-                    for sentence in content.findall("s"):
-                        lemma_sentence2 = lemmatize_sent(sentence)
-                        hashed_sentence2 = hash(lemma_sentence2)
-                        if hashed_sentence2 in short_sentence_dic and lemma_sentence2 not in final_sentences:
-                            final_sentences[lemma_sentence2] = short_sentence_dic[hashed_sentence2]
-                        else:
-                            pass
+            for _, sentence in ET.iterparse(xml_file, tag='s'):
+                lemma_sentence2 = lemmatize_sent(sentence)
+                hashed_sentence2 = hash(lemma_sentence2)
+                #checks if the hash of the file is in the list of the most common sentences
+                if hashed_sentence2 in short_sentence_dic and lemma_sentence2 not in final_sentences:
+                    final_sentences[lemma_sentence2] = short_sentence_dic[hashed_sentence2]
+                else:
+                    pass
+                sentence.clear()
 
     final_sentence_list_sorted = sorted(final_sentences.items(), key=lambda x: x[1])
 
+    #the sentences and their occurrences are written to the file
+    temp = ""
+    for entry in reversed(final_sentence_list_sorted):
+        temp += str(entry[0]) + " || " + str(entry[1]) + "\n"
+
     with open(outfile, 'w', encoding='utf-8') as output_file:
-        for entry in reversed(final_sentence_list_sorted):
-            temp = ""
-            temp += str(entry[0]) + " || " + str(entry[1]) + "\n"
             output_file.write(temp)
 
 def lemmatize_sent(sentence):
+    """
+    turns the raw text of the xml file into a lemmatized sentence
+    """
     sent = ""
-    for word in sentence.findall("w"):
+    for word in sentence.iterfind("w"):
         try:
             sent += word.attrib["lemma"] + " "
         except KeyError:
@@ -72,7 +83,10 @@ def lemmatize_sent(sentence):
     return sent
 
 def main():
-    filepath = "/Users/luc/Documents/PCL II/Exercises/exercise4/SAC"
+    """
+    Change filepath to the desired filepath
+    """
+    filepath = "SAC"
     outputfile = "output.txt"
 
     getfreqwords(filepath, outputfile)
